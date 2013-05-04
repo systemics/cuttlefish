b53d19904147baa377802aa8e6c2e60685cf197b
Karl Yerkes <karl.yerkes@gmail.com>
2013-05-04 06:33:50 -0700

    timer c++ class, almost

--- code/scratch/timer/timer.cpp
+++ code/scratch/timer/timer.cpp
@@ -4,20 +4,29 @@
 #include <assert.h>
 #include <stdio.h>
 
-void sig_handler(int signo) {
-  printf("GOT HERE\n");
+struct Foo {
+  void print() {
+    printf("got here\n");
+  }
+};
+
+void function(sigval_t s) {
+  ((Foo*)(s.sival_ptr))->print();
 }
 
 int main() {
-  assert(signal(SIGUSR1, sig_handler) != SIG_ERR);
-
-  struct sigevent sevp;
-  sevp.sigev_notify = SIGEV_SIGNAL;
-  sevp.sigev_signo = SIGUSR1;
-
   timer_t timerid;
+  struct itimerspec new_value = {{0, 16666666}, {1, 0}};
+  struct sigevent sevp;
+
+  Foo foo;
+
+  sevp.sigev_notify = SIGEV_THREAD;
+  sevp.sigev_value.sival_ptr = (void*)&foo;
+  sevp.sigev_notify_function = function;
+  sevp.sigev_notify_attributes = 0; // important
+
   assert(timer_create(CLOCK_REALTIME, &sevp, &timerid) == 0);
-  const struct itimerspec new_value = {{0, 16666666}, {1, 0}};
   assert(timer_settime(timerid, 0, &new_value, 0) == 0);
 
   for (;;) {
