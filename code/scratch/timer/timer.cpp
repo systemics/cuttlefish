adf399947a7ce3a3a207428a29fd0fc85d54ae90
Karl Yerkes <karl.yerkes@gmail.com>
2013-05-04 05:19:52 -0700

    timer working btu unfinished

--- code/scratch/timer/timer.cpp
+++ code/scratch/timer/timer.cpp
@@ -1,19 +1,24 @@
 #include <time.h>
 #include <signal.h>
 #include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+
+void sig_handler(int signo) {
+  printf("GOT HERE\n");
+}
 
 int main() {
-  // int timer_create(clockid_t clockid, struct sigevent *sevp, timer_t *timerid);
-  // int timer_settime(timer_t timerid, int flags, const struct itimerspec *new_value, struct itimerspec * old_value);
-
-  int result = 0;
+  assert(signal(SIGUSR1, sig_handler) != SIG_ERR);
 
   struct sigevent sevp;
-  timer_t timerid;
-  result = timer_create(CLOCK_REALTIME, &sevp, &timerid);
+  sevp.sigev_notify = SIGEV_SIGNAL;
+  sevp.sigev_signo = SIGUSR1;
 
+  timer_t timerid;
+  assert(timer_create(CLOCK_REALTIME, &sevp, &timerid) == 0);
   const struct itimerspec new_value = {{0, 16666666}, {1, 0}};
-  result = timer_settime(timerid, 0, &new_value, 0);
+  assert(timer_settime(timerid, 0, &new_value, 0) == 0);
 
   for (;;) {
     sleep(1);
