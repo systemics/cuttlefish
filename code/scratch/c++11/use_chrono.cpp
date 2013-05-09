6a3ee46b7d645fb5dabba1f54f8fbaeb91d747bd
Karl Yerkes <karl.yerkes@gmail.com>
2013-05-08 23:13:00 -0700

    tested stopwatch

--- code/scratch/c++11/use_chrono.cpp
+++ code/scratch/c++11/use_chrono.cpp
@@ -2,85 +2,15 @@
 #include <chrono>
 #include <unistd.h>
 
-namespace ctl {
-
-using namespace std;
-using namespace std::chrono;
-
-template <
-  typename UNIT = float,
-  typename CLOCK = high_resolution_clock,
-  int N = 10
->
-struct Stopwatch {
-  typename CLOCK::time_point start;
-  typename CLOCK::time_point stop;
-  duration<UNIT> elapsed_time;
-
-  void tic() {
-    start = CLOCK::now();
-  }
-
-  void toc() {
-    stop = CLOCK::now();
-    elapsed_time = duration_cast<duration<UNIT>>(stop - start);
-  }
-
-  void report() {
-    cout << elapsed_time.count() << N << endl;
-  }
-};
-
-}
-//#include "ctl_stopwatch.h"
-
-//struct Stopwatch {
-//  std::chrono::high_resolution_clock::time_point start;
-//  std::chrono::high_resolution_clock::time_point stop;
-//  std::chrono::duration<double> elapsed_time;
-//
-//  void tic() {
-//    start = std::chrono::high_resolution_clock::now();
-//  }
-//
-//  void toc() {
-//    stop = std::chrono::high_resolution_clock::now();
-//    elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(stop - start);
-//  }
-//
-//  void report() {
-//    std::cout << elapsed_time.count() << std::endl;
-//  }
-//};
-
-//using namespace std;
-//using namespace std::chrono;
-//
-//template <typename CLOCK, typename UNIT>
-//struct Stopwatch {
-//  high_resolution_clock::time_point start;
-//  high_resolution_clock::time_point stop;
-//  duration<double> elapsed_time;
-//
-//  void tic() {
-//    start = high_resolution_clock::now();
-//  }
-//
-//  void toc() {
-//    stop = high_resolution_clock::now();
-//    elapsed_time = duration_cast<duration<double>>(stop - start);
-//  }
-//
-//  void report() {
-//    cout << elapsed_time.count() << endl;
-//  }
-//};
+#include "../../graphics/ctl/ctl_stopwatch.h"
 
 int main () {
   ctl::Stopwatch<> s;
-  s.tic();
-  usleep(1234);
-  s.toc();
+  for (int i = 0; i < 20; ++i) {
+    s.tic();
+    usleep(5000);
+    s.toc();
+  }
   s.report();
   return 0;
 }
