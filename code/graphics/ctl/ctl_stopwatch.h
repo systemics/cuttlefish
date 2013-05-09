f9f0decf43b0da98cca79bc840b5e72db6eb4b7c
Karl Yerkes <karl.yerkes@gmail.com>
2013-05-08 20:09:08 -0700

    millisecond stopwatch

--- code/graphics/ctl/ctl_stopwatch.h
+++ code/graphics/ctl/ctl_stopwatch.h
@@ -9,14 +9,38 @@ using namespace std;
 using namespace std::chrono;
 
 template <
+  typename DATA,
+  int N
+>
+struct Average {
+  DATA data[N];
+  DATA sum;
+  unsigned index;
+  Average() : sum(0), index(0) {}
+  DATA operator()() {
+    return sum / N;
+  }
+  void operator()(DATA d) {
+    sum += d;
+    sum -= data[index];
+    data[index] = d;
+    index++;
+    if (index == N)
+      index = 0;
+  }
+};
+
+
+
+template <
   typename UNIT = milliseconds,
   typename CLOCK = high_resolution_clock,
-  int N = 10
+  int N = 20
 >
 struct Stopwatch {
   typename CLOCK::time_point start;
   typename CLOCK::time_point stop;
-  duration<UNIT> elapsed_time;
+  Average<float, N> average;
 
   void tic() {
     start = CLOCK::now();
@@ -24,11 +48,12 @@ struct Stopwatch {
 
   void toc() {
     stop = CLOCK::now();
-    elapsed_time = duration_cast<duration<UNIT>>(stop - start);
+    UNIT elapsed_time = duration_cast<UNIT>(stop - start);
+    average(elapsed_time.count());
   }
 
   void report() {
-    cout << elapsed_time.count() << N << endl;
+    cout << average() << endl;
   }
 };
 
