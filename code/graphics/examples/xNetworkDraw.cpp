9dfe2ebad8f36531e56a413f8e443ebfebd4bffb
Karl Yerkes <karl.yerkes@gmail.com>
2013-09-13 13:19:02 +0200

    networking examples

--- code/graphics/examples/xNetworkDraw.cpp
+++ code/graphics/examples/xNetworkDraw.cpp
@@ -2,63 +2,61 @@
 #include "gfx/gfx_mbo.h"
 
 using namespace std;
-using namespace ctl; 
+using namespace ctl;
 
 struct MyApp : public App {
-        
-    //THIS IS THE APP SPECIFIC STUFF, 
-	MBO * mbo;  
-	 
-	Quat model;
-	//Frame model;
-	
-	float time; 
-	
-	//INSTANTIATE THE APPLICATION WITH WIDTH AND HEIGHT
-	MyApp() : App(30, 20), time(0){ 
-		init();
-	}                                  
-	
-	//INIITIALIZE ELEMENTS OF THE SCENE 
-	virtual void init(){
-		
-		//Make a Mesh and add vertices
-		Mesh mesh;
-		mesh.add ( -1, -1, 0).add();
-		mesh.add ( -1, 1, 0).add();
-		mesh.add ( 1, 1,  0).add();
-		mesh.add ( 1, -1, 0).add();  
-		
-				
-		mbo = new MBO( mesh );
-	}  
 
-	void onTouch(int n, int x, int y, int t){
-    cout << "xBasicDraw: " << n << endl;
+    //THIS IS THE APP SPECIFIC STUFF,
+  MBO * mbo;
+
+  Quat model;
+  //Frame model;
+
+  float time;
+  float x, y;
+
+  //INSTANTIATE THE APPLICATION WITH WIDTH AND HEIGHT
+  MyApp() : App(30, 20), time(0) {
+    init();
+    x = 0;
+    y = 0;
   }
-	
-	//THIS CHANGES THE POSITION OF THE CIRCLE
-	void update(){
-	   
-		time +=.01;
-		 
-		scene.model.rot() 	= Quat( sin( time) * PI, Vec3f(1,0,0) );
-		scene.camera.rot()  = Quat( sin( time) * PI, Vec3f(0,1,0) );
-		
-		// mbo -> mesh.rotateA( Quat( sin(time), Vec3f(0,1,0) ) );
-		// mbo -> mesh.translate( .05, 0, 0 );
-		// mbo -> update(); 
 
-	}
+  //INIITIALIZE ELEMENTS OF THE SCENE
+  virtual void init(){
 
-	//THIS DRAWS THE CIRCLE TO THE SCREEN
-	virtual void onDraw(){ 
-		 
-		pipe.line( *mbo );
-	
-	} 
-	
-}; 
+    //Make a Mesh and add vertices
+    Mesh mesh;
+    mesh.add ( -1, -1, 0).add();
+    mesh.add ( -1, 1, 0).add();
+    mesh.add ( 1, 1,  0).add();
+    mesh.add ( 1, -1, 0).add();
+
+
+    mbo = new MBO( mesh );
+  }
+
+  void onTouch(int finger, int xPosition, int yPosition, int orientation){
+    x = xPosition / 3000.0;
+    y = yPosition / 2500.0;
+  }
+
+  void update() {
+    time +=.01;
+
+    //scene.model.rot()   = Quat( sin( time) * PI, Vec3f(1,0,0) );
+    //scene.camera.rot()  = Quat( sin( time) * PI, Vec3f(0,1,0) );
+
+    mbo -> mesh.rotateA( Quat( sin(x), Vec3f(0,1,0) ) );
+    mbo -> mesh.translate( y, 0, 0 );
+    mbo -> update();
+  }
+
+  //THIS DRAWS THE CIRCLE TO THE SCREEN
+  virtual void onDraw() {
+    pipe.line( *mbo );
+  }
+};
 
 STARTANDRUN()
 
