43d9c13b2362cc97843c2252c13ce2475514f8f4
Karl Yerkes <karl.yerkes@gmail.com>
2013-09-18 16:41:06 +0200

    added simple gamma sound

--- code/graphics/examples/xElectricField.cpp
+++ code/graphics/examples/xElectricField.cpp
@@ -1,106 +1,137 @@
 #include "ctl_appR2T.h"
-#include "vsr/vsr.h" 
+#include "vsr/vsr.h"
 #include "vsr/vsr_field.h"
 //#include "gfx/gfx_mbo.h"
 
+#include "Gamma/Gamma.h"    // core functions
+#include "Gamma/Filter.h"
+#include "Gamma/Noise.h"
+#include "Gamma/Oscillator.h"
+#include "Gamma/SamplePlayer.h"
+
 using namespace std;
-using namespace ctl; 
+using namespace ctl;
 using namespace vsr;
 
-struct MyApp : public AppR2T {     
-	 
+struct MyApp : public AppR2T {
+	
 	Field<Vec2D> vf;  	/// THE VECTOR FIELD
-	Field<Vec2D> orth; 	/// THE ORTHOGONAL VECTOR FIELD  
+	Field<Vec2D> orth; 	/// THE ORTHOGONAL VECTOR FIELD
 	
 	Field< Sca > rf;    /// Div and Curl of the Field
-	Field< Sca > gf;    /// Div and Curl of the Field   
-	Field< Sca > bf;    /// Div and Curl of the Field   
+	Field< Sca > gf;    /// Div and Curl of the Field
+	Field< Sca > bf;    /// Div and Curl of the Field
 	
 	int numDipoles;  	/// NUMBER OF DIPOLES TO CALCULATE
-	Vec2f * touch; 	    /// AN ARRAY OF TOUCHES 
-	Pnt * dipole; 
-	Par * par;   		/// DIPOLES THEMSELVES    
+	Vec2f * touch; 	    /// AN ARRAY OF TOUCHES
+	Pnt * dipole;
+	Par * par;   		/// DIPOLES THEMSELVES
+
+  gam::NoisePink<> noise;
+  gam::Biquad<> biquad;
+  gam::SineD<> bonk;
 	
-	int numPotentials; 
+	int numPotentials;
 	int pRes;
 	float step;
-	MBO * potentials;  
-	MBO * potentialsB; 
+	MBO * potentials;
+	MBO * potentialsB;
 	
     //a time element
 	float time;
+	float gain, res, cutoff;
 	
 	MyApp() : AppR2T( Layout(1,4) ),  //( 21.5,14.5),//
 		time(0),
 		vf(44,7,1,2),
-		orth(44,7,1,2), 
+		orth(44,7,1,2),
 		rf(44,7,1,2),
 		gf(44,7,1,2),
 		bf(44,7,1,2),
 		numDipoles(5),
-		numPotentials(10), 
+		numPotentials(10),
 		pRes(20),
 		step(.2)
-		 
+		
 	
-	{ 
-		AppR2T::init(); 
-        
+	{
+		AppR2T::init();
+		Sound::init();
+    gam::Sync::master().spu(48000); // XXX!!!
+
+
 		touch = new Vec2f[numDipoles];
-		dipole = new Pnt[numDipoles]; 
-		par = new Par[numDipoles];  
-		potentials = new MBO[numPotentials]; 
-		potentialsB = new MBO[numPotentials];  
+		dipole = new Pnt[numDipoles];
+		par = new Par[numDipoles];
+		potentials = new MBO[numPotentials];
+		potentialsB = new MBO[numPotentials];
 				
-		for (int i = 0; i < numPotentials; ++i){  
-			 potentials[i]   = MBO( Mesh::Contour(pRes).color(0,1,0).mode(GL::LS) );    
-			 potentialsB[i]  = MBO( Mesh::Contour(pRes).color(0,1,0).mode(GL::LS) );    
+		for (int i = 0; i < numPotentials; ++i){
+			 potentials[i]   = MBO( Mesh::Contour(pRes).color(0,1,0).mode(GL::LS) );
+			 potentialsB[i]  = MBO( Mesh::Contour(pRes).color(0,1,0).mode(GL::LS) );
 		}
 		
-
-	}  
+    gain = 0.5f;
+    cutoff = 500.0f;
+    res = 1.0f;
+	}
 	
-  void onTouch(int finger, int xPosition, int yPosition, int orientation) {     
+  void onTouch(int finger, int xPosition, int yPosition, int orientation) {
 
 		if (finger < numDipoles){
 			
 			float tx = xPosition / 3600.0;
-		    float ty = yPosition / -2600.0;    
+		    float ty = yPosition / -2600.0;
 		
 			touch[finger] = Vec2f(tx,ty);
-			 
+			
 			dipole[finger] = Point( layout.totalWidth()/2.0 * tx, layout.totalHeight()/2.0 * ty, 0 );
-			  
 		}
-	
-	}   
+
+    if (finger == 0) {
+      gain = (touch[0].y * 0.5f) + 0.5f;
+      cutoff = ((touch[0].x * 0.5f) + 0.5f) * 1100.0f;
+      res = 2;
+    }
+	}
+
+  virtual void onSound( SoundData& io ){
+    for (int i = 0; i < io.n; ++i) {
+      biquad.freq(cutoff);
+      biquad.res(res);
+      float s = biquad(noise()) * gain;
+      for (int j = 0; j < 2; j++) {
+        *io.outputBuffer++ = (short)(s * 32767.0);
+      }
+    }
+  }
 	
 	//Initialize
-	void initMeshes(){}  
+	void initMeshes(){}
 	
 	//Update the Geometry
-	void update(){   
-		                    
-		time += .05; 
+	void update(){
+		
+		time += .05;
 		
 		calcDipoles();
 		
-		calcVecFields();  
+		calcVecFields();
 		
 		calcEquipotentials();
-		 
+		
 		//colorMeshes();
 		
 		calcDivCurl();
-	}   
+	}
 	
 	void calcDivCurl(){
 		
-		for (int i = 0; i<vf.num(); ++i){  
+		for (int i = 0; i<vf.num(); ++i){
 			auto a = E1 * vf[i];
 			rf[i] = a[0];
 			gf[i] = a[1];
-		}                
+		}
 		
 		for (int i = 0 ; i < gf.num(); ++i){
 			bf[i] = gf.sumNbrs(i) / 4.0;
@@ -110,17 +141,17 @@ struct MyApp : public AppR2T {
 	//get field info at speaker locations
 	void pollData(){
 
-		Vec2D ld = vf.range( Vec2D(speakerL[0], speakerL[1]) ); // bounded 
-		Vec2D rd = vf.range( Vec2D(speakerR[0], speakerR[1]) ); // bounded  
+		Vec2D ld = vf.range( Vec2D(speakerL[0], speakerL[1]) ); // bounded
+		Vec2D rd = vf.range( Vec2D(speakerR[0], speakerR[1]) ); // bounded
 		
 		Patch lp = vf.surfIdx( ld[0], ld[1] );
-		Patch rp = vf.surfIdx( rd[0], rd[1] );   
+		Patch rp = vf.surfIdx( rd[0], rd[1] );
 		
 		//calculate div and curl upon patch
 		auto a = E1 * vf[lp.a];
-		auto b = E1 * vf[lp.b];   
-	    auto c = E1 * vf[lp.c];   
-	    auto d = E1 * vf[lp.d]; 
+		auto b = E1 * vf[lp.b];
+	    auto c = E1 * vf[lp.c];
+	    auto d = E1 * vf[lp.d];
 				
 	}
 	
@@ -128,75 +159,75 @@ struct MyApp : public AppR2T {
 		for (int i = 0; i < numDipoles; ++i){
 			//float t =  1. + 1.0 * i/numDipoles;
 			//touch[i] = Point( sin(time*t) * layout.totalWidth()/2.0, 0, 0 );
-			par[i] = Par(Tnv(0, 1.0 / numDipoles  ,0)).trs( dipole[i]  ); 
+			par[i] = Par(Tnv(0, 1.0 / numDipoles  ,0)).trs( dipole[i]  );
 		}
-	}  
+	}
 	
 	void calcVecFields(){
 		 for (int i = 0; i < vf.num(); ++i){
-				static Par tpar; 
+				static Par tpar;
 				tpar.reset();
 				
 				//sum in respective dipoles
-				for (int j = 0; j < numDipoles; ++j ){ 
+				for (int j = 0; j < numDipoles; ++j ){
 					float sqd =  ( Ro::sqd( vf.grid(i), dipole[j] ) + .1 );
-					float dist = 1.0 / sqd;  
+					float dist = 1.0 / sqd;
 					tpar += par[j] * dist;
-				 }   
+				 }
 				
 			   // Vec np =  vf.grid(i).bst ( tpar );//Ro::loc( vf.grid(i).bst ( tpar ) );
 				Vec np = Ro::loc( vf.grid(i).bst ( tpar ) );
-				vf[i] = Vec( np - Vec(vf.grid(i)) ); 
+				vf[i] = Vec( np - Vec(vf.grid(i)) );
 			    orth[i] = vf[i].rot( Biv::xy * PIOVERFOUR );
 			}
-	}  
+	}
 	
 	void calcEquipotentials(){
 		
-		for (int i = 0; i < 10; ++i){  
+		for (int i = 0; i < 10; ++i){
 			float theta = (TWOPI * i/numPotentials);
-			float ta = cos( theta ) * orth.tw()/2.0;  
-			float tb = sin( theta ) * orth.th()/2.0; 
+			float ta = cos( theta ) * orth.tw()/2.0;
+			float tb = sin( theta ) * orth.th()/2.0;
 			
-			// Vec nv(ta, tb, 0);  //starting point  
-			// Vec nvB(ta, tb, 0);  //starting point   
-			Vec2D nv(ta, tb);  //starting point  
+			// Vec nv(ta, tb, 0);  //starting point
+			// Vec nvB(ta, tb, 0);  //starting point
+			Vec2D nv(ta, tb);  //starting point
 			Vec2D nvB(ta, tb);  //starting point
 			
 			for (int j = 0; j < potentials[i].mesh.num(); ++j ){
 				//float tt = 1.0 * j/potentials[i].mesh.num();
 				potentials[i].mesh[j].Pos.set( nv[0], nv[1], 0 );     //2D
-			    //potentials[i].mesh[j].Col.set( 0,  1,  ta * .3, .9); 
+			    //potentials[i].mesh[j].Col.set( 0,  1,  ta * .3, .9);
 				potentialsB[i].mesh[j].Pos.set( nvB[0], nvB[1], 0 );  //2D
-			    //potentialsB[i].mesh[j].Col.set( 0,  1,  ta * .3, .9); 
+			    //potentialsB[i].mesh[j].Col.set( 0,  1,  ta * .3, .9);
 				nvB -= orth.euler2d( nvB ) * step;
-				nv += orth.euler2d( nv ) * step; 
-			}        
+				nv += orth.euler2d( nv ) * step;
+			}
 			
-			potentials[i].update(); 
+			potentials[i].update();
 			potentialsB[i].update();
 		}
-	}  
+	}
 	
 
 	
-    //DRAW GEOMETRY TO SCREEN  
+    //DRAW GEOMETRY TO SCREEN
 	 virtual void drawScene(){	
- 
-          
-	    // DRAWCOLOR( vf, 1,1,0 );    
+
+
+	    // DRAWCOLOR( vf, 1,1,0 );
 		Render( vf, mvm, pipe, rf, gf, bf );
 		//DRAWCOLOR( orth, 0,1,0);
 		
 		for (int i = 0; i < numPotentials; ++i){
 			pipe.line( potentials[i] );
 			pipe.line( potentialsB[i] );
-		}   
-		  
-		  
-	} 
+		}
+		
+		
+	}
 	
-}; 
+};
 
 bool running = true;
 void quit(int) {
@@ -210,7 +241,7 @@ int main(){
   while(running){
  		app.onFrame();
  		//usleep(1666);
- 	}   
+ 	}
 	
-	  return 0; 
+	  return 0;
 }
