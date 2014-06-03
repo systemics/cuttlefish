13ca839676af39195095b60fbd60df4864796182
Karl Yerkes <karl.yerkes@gmail.com>
2014-06-02 21:05:39 -0700

    added simple touch to example

--- examples/xElingsHall4x4-cuttlebone.cpp
+++ examples/xElingsHall4x4-cuttlebone.cpp
@@ -2,14 +2,34 @@
 
 struct Foo {
   float time;
+  int touchCount;
+  struct {
+    float x, y;
+  } touch[10];
 };
 
 #if SIM
 
-struct MyApp : ctl::Simulator<Foo> {
+#include "ctl_touch.h"
+
+using namespace ctl;
+
+struct MyApp : Simulator<Foo>, Touch {
   MyApp() : Simulator<Foo>("192.168.7.255", 1 / 30.f) {}
   virtual void setup(Foo& state) { state.time = 0; }
-  virtual void update(float dt, Foo& state) { state.time += 0.01f; }
+  virtual void update(float dt, Foo& state) {
+    state.time += 0.01f;
+
+    pollTouches();
+    state.touchCount = 0;
+    for (auto& e : touchPoint)
+      if (e.id != 0) {
+        state.touch[state.touchCount].x = e.x / 3200.0f;
+        state.touch[state.touchCount].y = e.y / -2600.0f;
+        state.touchCount++;
+      }
+    if (state.touchCount) LOG("x:%f y:%f", state.touch[0].x, state.touch[0].y);
+  }
 };
 
 #else
@@ -27,22 +47,29 @@ struct MyApp : CuttleboneApp<Foo> {
 
   MyApp() : CuttleboneApp<Foo>(Layout(4, 4), 30.0) {}
 
-  virtual void setup() { circle = new MBO(Mesh::Circle(6)); }
-
-  virtual void update(float dt, Foo& state, int popCount) {
-
-    // change position of vertices
-    circle->mesh.moveTo(sin(state.time) * layout.totalWidth() / 2.0,
-                        cos(state.time) * layout.totalHeight() / 2.0, 0);
-    circle->mesh.color(1, fabs(sin(state.time)), 1);
-
-    // send changes to buffer
-    circle->update();
-
+  virtual void setup() {
+    circle = new MBO(Mesh::Circle(6));
     mboR = new MBO(Mesh::Num(identifier.row).translate(-1, 0, 0).moveTo(
         layout.speakerL + Vec3f(3, 0, 0)));
     mboC = new MBO(Mesh::Num(identifier.col).translate(1, 0, 0).moveTo(
         layout.speakerL + Vec3f(layout.screenWidth - 3, 0, 0)));
+  }
+
+  virtual void update(float dt, Foo& state, int popCount) {
+
+    static float x, y;
+
+    if (state.touchCount) {
+      x = state.touch[0].x;
+      y = state.touch[0].y;
+    }
+
+    circle->mesh.moveTo(x * layout.totalWidth() / 2.0,
+                        y * layout.totalHeight() / 2.0, 0);
+    circle->mesh.color(1, fabs(sin(state.time)), 1);
+
+    // send changes to buffer
+    circle->update();
 
     // background color
     background.set(fabs(sin(state.time)) * .2, fabs(cos(state.time)) * 1, 1, 1);
