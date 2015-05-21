d4c019421e5f2013b80ede82aab27498cebe3f85
wolftype <wolftype@gmail.com>
2015-05-21 14:01:14 -0700

    cmake build redux

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+HOST=jetson
+
 DIRECTORY=`dirname $1`
 TARGET=`basename $1|cut -d'.' -f1 | sed -e "s|/|_|g"`
 
-HOST=fixme
-#10.0.1.66 #hmmm, could init_session to set this (call init_session.sh 10.0.1.66)
+echo Deploying $TARGET in $DIRECTORY to jetson:~/share folder
 
-echo DEPLOYING $TARGET to $HOST
-ssh -p 22 $HOST  "rm -f /tmp/$TARGET"
-scp -P 22 build/pi/$TARGET $HOST:/tmp
-echo RUNNING $TARGET on $HOST
-ssh -t -p 22 $HOST /tmp/$TARGET
+ssh -p 22 $HOST  "rm -f ~/share/$TARGET"
+scp -P 22 build/pi/$TARGET $HOST:~/share
+
+#echo RUNNING $TARGET on $HOST
+#ssh -t -p 22 $HOST /tmp/$TARGET
